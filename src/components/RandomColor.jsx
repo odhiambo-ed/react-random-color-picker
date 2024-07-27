167b0df382cbfef44ad373351b6c54c0181da667
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-27 23:47:09 +0300

    add random rgb color function

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -5,9 +5,18 @@ function RandomColor() {
     const [randomRgb, setRandomRgb] = useState("rgb(255,255,255)")
 
     const handleRgbClick = () => {
-        const r = Math.floor(Math.random() * 256).toString(16)
-        console.log(r)
-        return setRandom(randomNumber)
+        const rgb = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)})`;
+        setRandomRgb(rgb)
+    }
+
+    const handleHexClick = () => {
+
+    }
+
+    const handleHslClick = () => {
+
     }
   return (
     <div className="container vw-100 vh-100 d-flex flex-column gap-2 justify-content-center align-items-center">
@@ -23,9 +32,9 @@ function RandomColor() {
           Generate With HSL
         </div>
       </div>
-      <div className="btn btn-outline-dark py-2 px-5">Color: rgb({randomRgb}) </div>
-      <div className="btn btn-outline-dark py-2 px-5">Color: #{randomHex} </div>
-      <div className="btn btn-outline-dark py-2 px-5">Color: hsl({randomHsl}) </div>
+      <div className="btn btn-outline-dark py-2 px-5">Color: {randomRgb} </div>
+      {/* <div className="btn btn-outline-dark py-2 px-5">Color: #{randomHex} </div>
+      <div className="btn btn-outline-dark py-2 px-5">Color: hsl({randomHsl}) </div> */}
     </div>
   );
 }
