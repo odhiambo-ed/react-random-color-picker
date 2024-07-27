b927663c353392ebc4c4cf19a82633b47942a430
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-27 21:31:37 +0300

    import random color

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import RandomColor from './components/RandomColor'
 
 function App() {
 
   return (
     <>
-      <h1>Jesus is always great for me</h1>
+      <RandomColor />
     </>
   )
 }
