01620dfb66437b4268e5ee13e834db5fc5ef38b7
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-28 00:12:00 +0300

    add color type state

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 
 
 function RandomColor() {
-    const [randomRgb, setRandomRgb] = useState("rgb(255,255,255)")
+    const [randomColor, setRandomColor] = useState("rgb(255,255,255)")
+    const [colorType, setColorType] = useState("rgb")
 
     const handleRgbClick = () => {
         const rgb = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
           Math.random() * 256
         )}, ${Math.floor(Math.random() * 256)})`;
-        setRandomRgb(rgb)
+        setRandomColor(rgb)
     }
 
     const handleHexClick = () => {
@@ -19,20 +20,20 @@ function RandomColor() {
 
     }
   return (
-    <div style={{backgroundColor: randomRgb}} className="container-fluid vw-100 vh-100 d-flex flex-column gap-2 justify-content-center align-items-center">
+    <div style={{backgroundColor: randomColor}} className="container-fluid vw-100 vh-100 d-flex flex-column gap-2 justify-content-center align-items-center">
       <h3>Generate Random Colors</h3>
       <div className="d-flex gap-1">
-        <div onClick={handleRgbClick} className="btn btn-outline-dark">
+        <div onClick={() => setColorType("rgb")} className="btn btn-outline-dark">
           Generate With RGB
         </div>
-        <div onClick={handleHexClick} className="btn btn-outline-dark">
+        <div onClick={() => setColorType("hex")} className="btn btn-outline-dark">
           Generate With Hexadecimal
         </div>
-        <div onClick={handleHslClick} className="btn btn-outline-dark">
+        <div onClick={() => setColorType("hsl")} className="btn btn-outline-dark">
           Generate With HSL
         </div>
       </div>
-      <div className="btn btn-outline-dark py-2 px-5">Color: {randomRgb} </div>
+      <div className="btn btn-outline-dark py-2 px-5">Color: {randomColor} </div>
       {/* <div className="btn btn-outline-dark py-2 px-5">Color: #{randomHex} </div>
       <div className="btn btn-outline-dark py-2 px-5">Color: hsl({randomHsl}) </div> */}
     </div>
